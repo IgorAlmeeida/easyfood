2eaac0eb908c7b854d2fee0141af7f14d43ce66d
Igor Almeida <igor.almeida@libertyti.com.br>
2026-01-20 16:49:51 -0300

    Adicionando processo sonar e ajustes nas regras de negócio.

--- src/test/java/br/com/imsa/easyfood/EasyfoodApplicationTests.java
+++ src/test/java/br/com/imsa/easyfood/EasyfoodApplicationTests.java
@@ -3,10 +3,35 @@ package br.com.imsa.easyfood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class EasyfoodApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:16")
+    ).withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+
+        // mantém liquibase habilitado
+        registry.add("spring.liquibase.enabled", () -> true);
+    }
 
 	@Test
 	void contextLoads() {
